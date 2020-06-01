e75b11e2947fd74a4dcc701b981cb666a176d724
Jonathan Adshead <jonathan.adshead@aexp.com>
2020-06-01 16:37:54 -0700

    feat(make-promises-safe): treat unhandledRejection as uncaught exception

--- src/server/init.js
+++ src/server/init.js
@@ -14,6 +14,7 @@
  * permissions and limitations under the License.
  */
 
+import 'make-promises-safe';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'cross-fetch/polyfill';
